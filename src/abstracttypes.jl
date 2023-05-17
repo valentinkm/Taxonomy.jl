9560032bb6de764e6104ec30d8ce03c3daa74ab4
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-05-17 13:24:22 +0200

    hierarchy conceptually only in abstracttypes.jl

--- src/abstracttypes.jl
+++ src/abstracttypes.jl
@@ -4,6 +4,35 @@ abstract type AbstractMeta end
 """
 Taxon is the supertype of all taxons.
 """
+
 abstract type Taxon end
-abstract type Pathmodel <: Taxon end
-abstract type AbstractFactor <: Taxon end
+abstract type NoTaxon end
+abstract type CFA <: Taxon end
+abstract type AbstractSEM <: Taxon end
+
+
+struct Pathmodel <: Taxon end
+struct SimpleCFA <: CFA end
+struct HierarchicalCFA <: CFA end
+struct CrossSectionalSEM <: SEM end
+struct LongitudinalSEM <: SEM end
+
+#composite:
+struct CFAFactor <: CFA end
+    type1::SimpleCFA
+    type2::HierarchicalCFA
+
+struct SEMFactor <: AbstractSEM end
+    type1::CrossSectionalSEM
+    type2::LongitudinalSEM
+
+# multiple dispatch:
+function CFAFactor(factor::CFAFactor)
+    # do something with SimpleCFA and HierarchicalCFA
+    println("Doing something with CFAFactor")
+end
+
+function SEMFactor(factor::SEMSector)
+    # do something with CrossSectionalSEM and LongitudinalSEM
+    println("Doing something with SEMFactor")
+end

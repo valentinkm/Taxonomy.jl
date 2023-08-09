980cbfd90ce0e8ba8e1f409b0d9c8f1a211a9e2f
valentinkm <valentin.kriegmair@student.hu-berlin.de>
2023-08-09 11:54:27 +0200

    union type for no access

--- src/record.jl
+++ src/record.jl
@@ -3,7 +3,7 @@ struct Record
     id::Union{Base.UUID, Missing}
     location::AbstractLocation
     meta::AbstractMeta
-    no_access::Bool
+    no_access::Union{Bool, Missing}
     taxons::Union{Vector{<: Taxon}, Missing}
     spec::Union{Judgement, Missing}
     data::Union{Judgement, Missing}
